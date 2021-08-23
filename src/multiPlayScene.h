8d54739b985b461e052c88515809a22344126287
Rokr0k <yangeh2225@gmail.com>
2021-08-23 16:40:08 +0900

    aseotnuhaoesuhaosenuh

--- src/multiPlayScene.h
+++ src/multiPlayScene.h
@@ -6,6 +6,6 @@ void MultiPlayScene_Event(SDL_Event* event);
 void MultiPlayScene_Update(double delta);
 void MultiPlayScene_Render(SDL_Renderer* renderer);
 void MultiPlayScene_Quit();
-void MultiPlayScene_SetPeer(IPaddress);
+void MultiPlayScene_SetPeer(IPaddress address);
 
 #endif
